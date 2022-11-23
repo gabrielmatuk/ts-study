2ed90e71308fde0300c11726cf296e162b53e43f
gabrielmatuk <gabrielmatuk14@gmail.com>
2022-11-23 20:48:18 -0300

    feat(login): new screen login

--- 02-React/ts-react/src/App.tsx
+++ 02-React/ts-react/src/App.tsx
@@ -1,21 +1,27 @@
 import {ButtonComponent} from './components/Button';
 import {Inputs} from './components/Inputs';
 import { Layout } from './components/Layout';
-import { ChakraProvider, Center} from '@chakra-ui/react'
+import { ChakraProvider, Box,Center } from '@chakra-ui/react'
 
 
 function App() {
   return (
     <>
     <ChakraProvider>
-    <Layout>
-      <Center>
-      <h1>Faça o Login:</h1>
-      <Inputs id='emailInput' label='Email:' htmlFor='emailInput'></Inputs>
-      <Inputs id='passwordInpput' label='Senha:' htmlFor='passwordInpput' type='password'></Inputs>
-      <ButtonComponent />
-      </Center>
-    </Layout>
+        <Box minHeight='100vh' backgroundColor='#4d4d4d' padding='25px'>
+          <Box backgroundColor='#fafafa' borderRadius='25px' padding='15px'>
+          <Center>
+
+          <h1>Faça o Login:</h1>
+          </Center>
+
+          <Inputs id='emailInput' label='Email:' htmlFor='emailInput'></Inputs>
+          <Inputs id='passwordInpput' label='Senha:' htmlFor='passwordInpput' type='password'></Inputs>
+             <Center>
+              <ButtonComponent />
+            </Center>
+          </Box>
+        </Box>
     </ChakraProvider>
     </>
   );
--- 02-React/ts-react/src/components/Button.tsx
+++ 02-React/ts-react/src/components/Button.tsx
@@ -2,9 +2,10 @@ import {Button}  from '@chakra-ui/react'
 
 
 export const ButtonComponent = () => {
+
   return (
     <>
-    <Button>Entrar</Button>
+    <Button size='lg' colorScheme='blackAlpha' paddingTop='5px' marginTop='15px' width='60%' >Entrar</Button>
     </>
     )
 }
