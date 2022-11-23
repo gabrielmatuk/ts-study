9e11c991eae48b367b17f4a8f50298578e1e14eb
gabrielmatuk <gabrielmatuk14@gmail.com>
2022-11-23 00:44:39 -0300

    privatingfunctions

--- 01-BankAccount/class/BankAccount.ts
+++ 01-BankAccount/class/BankAccount.ts
@@ -1,11 +1,16 @@
 export abstract class BankAccount {
-    name: string
+    private name: string
     accountNumber: number
     balance: number = 0
 
     constructor(name: string, accountNumber: number){
         this.name = name
         this.accountNumber = accountNumber
+    }
+
+    setName = (name: string):void => {
+        this.name = name
+        console.log('Nome alterado com sucesso')
     }
 
     deposit = (): void => {
