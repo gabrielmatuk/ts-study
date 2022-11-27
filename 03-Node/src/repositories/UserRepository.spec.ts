34b693c50165f6eadd3ee647b539bddebcd28f00
gabrielmatuk <gabrielmatuk14@gmail.com>
2022-11-27 19:13:06 -0300

    test(userRepository): creating test

---
+++ 03-Node/src/repositories/UserRepository.spec.ts
@@ -0,0 +1,26 @@
+import { EntityManager } from "typeorm"
+import { User } from "../entities/User"
+import { getMockEntityManager } from "../__mocks__/mockEntityManager.mock"
+import { UserRepository } from "./UserRepository"
+
+describe('UserRepository', () => {
+    let userRepository: UserRepository
+    let managerMock: Partial<EntityManager>
+
+    const mockUser:User ={
+        user_id: '12345',
+        name:'Teste',
+        email:'teste@test.com',
+        password: 'senha'
+    } 
+
+    beforeAll(async () => {
+        managerMock = await getMockEntityManager({})
+        userRepository = new UserRepository(managerMock as EntityManager)
+    })
+
+    it('Deve cadastrar um novo usuário no banco de dados', async()=>{
+        await userRepository.createUser(mockUser)
+        expect(managerMock.save).toHaveBeenCalled()
+    })
+})
