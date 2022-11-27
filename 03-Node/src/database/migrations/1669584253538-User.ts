83a69ab48435a8964b130a8005e64ef4c1d599fa
gabrielmatuk <gabrielmatuk14@gmail.com>
2022-11-27 18:24:29 -0300

    feat(db): add sqlite and typeorm

---
+++ 03-Node/src/database/migrations/1669584253538-User.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class User1669584253538 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- 03-Node/src/index.ts
+++ 03-Node/src/index.ts
@@ -1,11 +1,23 @@
 import express, { Response} from 'express';
 import { router } from './routes';
+import 'reflect-metadata'
+import { AppDataSource } from './database';
 
 const app = express()
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+    
 app.use(express.json())
 app.use(router)
 
 const PORT = 5555
+
 
 app.get('/', (_, res: Response) => {
     return res.status(200).json({msg: true})
