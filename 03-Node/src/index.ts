d031c1eb546e902c8a352adc187de6a19a1fb54c
gabrielmatuk <gabrielmatuk14@gmail.com>
2022-11-27 01:21:45 -0300

    feat(node): starting project

--- 03-Node/src/index.ts
+++ 03-Node/src/index.ts
@@ -1,1 +1,19 @@
-console.log('a')
+import express, {Request, Response} from 'express';
+
+const app = express()
+app.use(express.json())
+
+const PORT = 5555
+
+app.get('/', (_, res: Response) => {
+    return res.status(200).json({msg: true})
+})
+
+app.post('/', (req:Request, res: Response) => {
+    const body = req.body
+    return res.status(200).json({msg: true})
+})
+
+app.listen(PORT, () => {
+console.log('Server Started.')
+})
